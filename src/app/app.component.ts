afdc969ac805b2cb425bf5a3b45fc621170e311f
a6hinavram <a6hinavram@gmail.com>
2020-11-25 12:23:51 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+ 
+  form: FormGroup;
+  payLoad = '';
+
+  ngOnInit() {
+    this.form = this.toFormGroup();
+  }
+
+  data = [
+  {
+        key: 'firstName',
+        label: 'First name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'lastName',
+        label: 'Last name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'gender',
+        label: 'Gender',
+        field_type : 'select',
+        required: true,
+        options: [
+          {key: 'male',  value: 'Male'},
+          {key: 'female',  value: 'Female'}
+        ],
+
+  },
+];
+
   title = 'my-first-app';  
 
+  toFormGroup() {
+    let group: any = {};
+
+    this.data.forEach(question => {
+      group[question.key] = question.required ? 
+      new FormControl(question.value || '', Validators.required)
+                                              : new FormControl(question.value || '');
+    });
+    return new FormGroup(group);
+  }
+
+ onSubmit() { 
+    //this.submitted = true;
+    this.payLoad = JSON.stringify(this.form.value);
+
+    alert("form submission test");
+  }  
 
 }
