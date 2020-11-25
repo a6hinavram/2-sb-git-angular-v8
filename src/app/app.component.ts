a443b16b2d51480b0cc32183c9b6a8749fe672a8
a6hinavram <a6hinavram@gmail.com>
2020-11-25 11:23:05 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { User }    from './User';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'my-first-app';  
+  genderOptions = ['Male', 'Female'];
+   submitted = false;
+
+  model = new User('', '', '', this.genderOptions[1]);
+  
+  onSubmit() { 
+    this.submitted = true;
+    alert("form submission test");
+  }  
+
+   // TODO: Remove this when we're done  
+  get diagnostic() { return JSON.stringify(this.model); }  
 
 
 }
