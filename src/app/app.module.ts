a1b725fa81b6960b7824cfcd26abda3cccf40b81
a6hinavram <a6hinavram@gmail.com>
2020-11-25 11:16:17 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,3 @@ import { FormsModule } from '@angular/forms';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
