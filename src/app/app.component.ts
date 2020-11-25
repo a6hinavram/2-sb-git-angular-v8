68140686608ee1457070c8ef5d166073cb89389c
a6hinavram <a6hinavram@gmail.com>
2020-11-25 11:37:11 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  name = new FormControl("test");
-  cars = new FormControl("mercedes-101");
+  constructor(private fb: FormBuilder) { }
 
-  updateName() {
-    this.name.setValue("Nancy");
-  }
+  profileForm = this.fb.group({
+  firstName: [''],
+  lastName: [''],
+  address: this.fb.group({
+    street: [''],
+    city: [''],
+    state: [''],
+    zip: ['']
+  }),
+  aliases: this.fb.array([
+    this.fb.control('')
+  ])
+});
+
+get aliases() {
+  return this.profileForm.get('aliases') as FormArray;
+}
+
+addAlias() {
+  this.aliases.push(this.fb.control(''));
+}
 }
