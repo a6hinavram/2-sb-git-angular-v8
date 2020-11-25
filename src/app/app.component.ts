14c7bd6f64bc5be7dd216a71216380b3866080ab
a6hinavram <a6hinavram@gmail.com>
2020-11-25 13:02:53 +0530

    1 file modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: 'my-app',
+  template: 
+  `
+    
+    <ng-container *ngIf="foo === 1;else second">
+    sdf
+    </ng-container>
+
+<ng-template #second>
+second template
+    <ng-container *ngIf="foo === 2;else third"></ng-container>
+</ng-template>
+<ng-template #third>
+third
+</ng-template>
+    
+    `,
 })
-export class AppComponent {
-  title = 'my-first-app';  
+export class AppComponent { 
 
-
+  foo = 3;
+  b = 2;
 }
