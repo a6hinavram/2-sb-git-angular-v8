e1e0cb6894e37a630798a3bc1f19986119d9cbb7
a6hinavram <a6hinavram@gmail.com>
2020-11-25 12:22:42 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import {formdata} from './formdata'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+ data = [
+  {
+        key: 'firstName',
+        label: 'First name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'lastName',
+        label: 'Last name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'gender',
+        label: 'Gender',
+        field_type : 'select',
+        required: true,
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+
+  },
+];
+
   title = 'my-first-app';  
 
+ onSubmit() { 
+    //this.submitted = true;
+    alert("form submission test");
+  }  
 
 }
