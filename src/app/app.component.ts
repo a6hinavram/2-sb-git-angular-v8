2ec642facf3ae5ad140a135aff8301ecc0fc123b
a6hinavram <a6hinavram@gmail.com>
2020-11-25 12:44:46 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  data = [
+  {
+        key: 'firstName',
+        label: 'First name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'lastName',
+        label: 'Last name',
+        field_type : 'text',
+        value : 'Bombasto',
+        required: true,
+
+  },
+  {
+        key: 'gender',
+        label: 'Gender',
+        field_type : 'select',
+        required: true,
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+
+  },
+];
+
   title = 'my-first-app';  
 
+ onSubmit() { 
+    //this.submitted = true;
+    alert("form submission test");
+  }  
 
 }
